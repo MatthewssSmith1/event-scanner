20f11dce268c4a7ea116306340267fdee31b29b2
Matt Smith <matthewsssmith1@gmail.com>
2022-02-17 16:00:14 -0500

    simplified UI code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import dynamic from "next/dynamic";
 import { useState } from "react";
-import { ArrowLeftIcon } from "@heroicons/react/solid";
+import { ArrowLeftIcon, HomeIcon } from "@heroicons/react/solid";
+const QrReader = dynamic(() => import("react-qr-reader"), { ssr: false });
 import cn from "classnames";
 
 import styles from "./index.module.css";
@@ -18,19 +19,31 @@ export type Err = {
   message: string;
 };
 
-const QrReader = dynamic(() => import("react-qr-reader"), { ssr: false });
+const exTicket: TicketState = {
+  data: "11/2 Jerry 4 78192976",
+  date: "11/2",
+  name: "Jerry",
+  num: "4",
+  lastUsed: 995000000000,
+  isNew: true,
+};
 
-export default function Home() {
-  const [ticket, setTicket] = useState<TicketState | null>(null);
-  // {
-  //   data: "2/16 Jerry 5 2189890",
-  //   date: "2/16",
-  //   name: "Jerry",
-  //   num: "5",
-  //   lastUsed: 10000000,
-  //   isNew: true
-  // }
-  const [err, setErr] = useState<string | null>();
+export default function Index() {
+  const [ticket, setTicket] = useState<TicketState | null>(exTicket);
+
+  return (
+    <div className={styles.content}>
+      {!ticket && <Home setTicket={setTicket} />}
+      {ticket && <TicketView ticket={ticket} setTicket={setTicket} />}
+    </div>
+  );
+}
+
+type HomeProps = {
+  setTicket: (ticket: TicketState | null) => void;
+};
+function Home({ setTicket }: HomeProps) {
+  const [err, setErr] = useState<string | null>(null);
 
   const onScan = async (data: string | null) => {
     if (!data) return;
@@ -43,49 +56,60 @@ export default function Home() {
     if (!res) return;
 
     const ticket: TicketState | Err = await res.json();
-    if (ticket as TicketState) setTicket(ticket as TicketState);
-    if (ticket as Err) setErr((ticket as Err).message);
+    if (ticket as Err) return setErr((ticket as Err).message);
+
+    if (!(ticket as TicketState)) return;
+
+    setTicket(ticket as TicketState);
+    setErr(null);
   };
 
-  var lastUsed = "never";
-  if (ticket as TicketState && !ticket?.isNew) {
-    var secondsElapsed = Math.ceil(
-      (Date.now() - (ticket as TicketState).lastUsed) / 1000
-    );
-    if (secondsElapsed < 60) lastUsed = `${secondsElapsed} seconds ago`;
-    else lastUsed = `${Math.ceil(secondsElapsed / 60)} minutes ago`;
-  }
+  return (
+    <>
+      <div className={styles.scanner}>
+        <QrReader onScan={onScan} onError={setErr} />
+      </div>
+      <div className={styles.homeInfo}>{err && <p>{err}</p>}</div>
+    </>
+  );
+}
 
+type TicketViewProps = {
+  ticket: TicketState;
+  setTicket: (ticket: TicketState | null) => void;
+};
+function TicketView({ ticket, setTicket }: TicketViewProps) {
+  const { date, name, num, isNew } = ticket;
 
   return (
-    <div className={styles.content}>
-      <nav>
-        <div onClick={() => setTicket(null)}>
-          <ArrowLeftIcon />
-        </div>
-      </nav>
-      {ticket && ticket.data && (
-        <>
-          <div className={styles.info}>
-            <h1>{ticket.date}</h1>
-            <h1>{ticket.name.replaceAll("_", " ")}</h1>
-            <h1>{ticket.num}</h1>
-          </div>
-          <div
-            className={cn(styles.lastUsed, {
-              [`${styles.isNew}`]: ticket.isNew,
-            })}
-          >
-            <h1>{lastUsed}</h1>
-          </div>
-        </>
-      )}
-      {!ticket && (
-        <div className={styles.scanner}>
-          <QrReader onScan={onScan} onError={setErr} />
-        </div>
-      )}
-      {err && <h1>{err}</h1>}
-    </div>
+    <>
+      <div className={styles.backArrow} onClick={() => setTicket(null)}>
+        <ArrowLeftIcon />
+      </div>
+      <div className={styles.info}>
+        <h1>{date}</h1>
+        <h1>{name.replaceAll("_", " ")}</h1>
+        <h1>{num}</h1>
+      </div>
+      <div
+        className={cn(styles.lastUsed, {
+          [`${styles.isNew}`]: isNew,
+        })}
+      >
+        <h1>{lastUsedText(ticket)}</h1>
+      </div>
+    </>
   );
+}
+
+function lastUsedText(t: TicketState) {
+  const { isNew, lastUsed } = t;
+
+  if (isNew) return "never";
+
+  var seconds = Math.ceil((Date.now() - lastUsed) / 1000);
+
+  if (seconds < 60) return `${seconds} seconds ago`;
+
+  return `${Math.ceil(seconds / 60)} minutes ago`;
 }
