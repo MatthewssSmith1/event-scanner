7324b3a056547a32230bde67a0e20795d4badf0b
Matt Smith <matthewsssmith1@gmail.com>
2022-10-28 19:09:39 -0400

    changed cors policya

--- pages/api/scan.ts
+++ pages/api/scan.ts
@@ -24,59 +24,60 @@ export default async function handler(
   req: any,// NextApiRequest,
   res: any//NextApiResponse<TRes | { message: string }>
 ) {
-  const { db } = (await connectToDatabase()) as { db: Db };
+  return res.status(405).json({ message: "stpp early" });
+  // const { db } = (await connectToDatabase()) as { db: Db };
   
-  // return res.status(200);
+  // // return res.status(200);
 
-  if (req.method !== "POST")
-    return res.status(405).json({ message: "Only POST requests allowed" });
+  // if (req.method !== "POST")
+  //   return res.status(405).json({ message: "Only POST requests allowed" });
 
-  let eventId, ticketData;
-  try {
-    console.log(req.body);
-    let body = req.body
+  // let eventId, ticketData;
+  // try {
+  //   console.log(req.body);
+  //   let body = req.body
 
-    // if (body.length !== 2)
-    //   return res.status(201).json({ message: "no ticket data provided" });
+  //   // if (body.length !== 2)
+  //   //   return res.status(201).json({ message: "no ticket data provided" });
 
-    ticketData = body.ticketData;
-    eventId = body.eventId;
-  } catch (e) {
-    console.log("caught", e);
-    return res.status(406).json({ message: "parse error" });
-  }
+  //   ticketData = body.ticketData;
+  //   eventId = body.eventId;
+  // } catch (e) {
+  //   console.log("caught", e);
+  //   return res.status(406).json({ message: "parse error" });
+  // }
 
-  if (!ticketData || ticketData.length === 0)
-    return res.status(201).json({ message: "no ticket data provided" });
+  // if (!ticketData || ticketData.length === 0)
+  //   return res.status(201).json({ message: "no ticket data provided" });
 
-  const ticket = stringToTicket(ticketData, process.env.HASH_SECRET);
+  // const ticket = stringToTicket(ticketData, process.env.HASH_SECRET);
 
-  if (ticket === undefined)
-    return res.status(202).json({ message: "invalid ticket" });
+  // if (ticket === undefined)
+  //   return res.status(202).json({ message: "invalid ticket" });
 
-  const ticketEventId = ticket.eventId; //.replace(" ", "_");
+  // const ticketEventId = ticket.eventId; //.replace(" ", "_");
 
-  if (ticketEventId !== eventId)
-    return res.status(203).json({
-      message: `wrong event: ${ticket.eventId} ${ticket.group} (${ticket.id})`,
-    });
+  // if (ticketEventId !== eventId)
+  //   return res.status(203).json({
+  //     message: `wrong event: ${ticket.eventId} ${ticket.group} (${ticket.id})`,
+  //   });
 
-  const found = await db.collection("tickets").findOne({ data: ticketData });
+  // const found = await db.collection("tickets").findOne({ data: ticketData });
 
-  if (found)
-    return res.status(200).json({
-      message: `${ticket.group} (${ticket.id})`,
-      elapsedTime: Date.now() - found.lastUsed,
-    } as TRes);
+  // if (found)
+  //   return res.status(200).json({
+  //     message: `${ticket.group} (${ticket.id})`,
+  //     elapsedTime: Date.now() - found.lastUsed,
+  //   } as TRes);
 
-  await db.collection("tickets").insertOne({
-    eventId,
-    data: ticketData,
-    lastUsed: Date.now(),
-  } as Entry);
+  // await db.collection("tickets").insertOne({
+  //   eventId,
+  //   data: ticketData,
+  //   lastUsed: Date.now(),
+  // } as Entry);
 
-  return res.status(200).json({
-    message: `${ticket.group} (${ticket.id})`,
-    elapsedTime: 0,
-  } as TRes);
+  // return res.status(200).json({
+  //   message: `${ticket.group} (${ticket.id})`,
+  //   elapsedTime: 0,
+  // } as TRes);
 }
